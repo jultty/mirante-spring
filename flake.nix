b7a313c517d845b8bd63d19480ee7c5d6bdbf492
jultty <juno.t@tuta.io>
2024-01-05 15:57:58 -0300

    flake: Properly inherit default system arch

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
-      inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
-
-    outputs = { self, nixpkgs }: 
-        let 
-            pkgs = import nixpkgs { system = "x86_64-linux"; };
-        in
-        {
-            devShells.x86_64-linux.default = pkgs.mkShell {
-                packages = [
-                    pkgs.jdk21
-                    pkgs.maven
-                ];
-                DEBUG = 1;
-            };
-        };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
+  outputs = { self, nixpkgs }: 
+    let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { 
+      inherit system; 
+    };
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        pkgs.jdk21
+        pkgs.maven
+      ];
+      DEBUG = 1;
+    };
+  };
 }
