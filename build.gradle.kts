10c86f48b826509e30252ce8bd6a0b773fcd4cd9
jultty <juno.t@tuta.io>
2024-01-06 14:41:57 -0300

    Add modernizer Gradle plugin, update CI action

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
+  id("com.github.andygoossens.modernizer") version "1.9.0"
 }
 
 group = "dev.jutty.mirante.server"
@@ -18,8 +19,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	runtimeOnly("com.h2database:h2")
 }
 
 tasks.withType<Test> {
