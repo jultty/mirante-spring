3af8a5976025e2e08cbc306b1e3a1af2d5dd0546
jultty <juno.t@tuta.io>
2024-01-06 04:08:25 -0300

    Drop Spring Security dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,10 +18,8 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
