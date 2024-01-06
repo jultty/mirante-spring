98a6ea4ae2fe6586d04ea92bbd0bd775edddc328
jultty <juno.t@tuta.io>
2024-01-06 02:37:04 -0300

    Setup flake build scaffold

--- flake.nix
+++ flake.nix
@@ -1,14 +1,38 @@
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11"; };
-  outputs = { self, nixpkgs }: 
+  inputs = { 
+    mvn2nix.url = "github:fzakaria/mvn2nix";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { nixpkgs, mvn2nix, utils, ... }: 
     let 
+    name = "mirante-spring";
+    version = "0.1.1-SNAPSHOT";
+    mainClass = "ApiApplication";
     system = "x86_64-linux";
     pkgs = import nixpkgs { 
       inherit system; 
     };
   in
   {
-    devShells.x86_64-linux.default = pkgs.mkShell {
+    # Package outputs
+
+    defaultPackage.${system} = 
+      with import nixpkgs { system = "${system}"; };
+      stdenv.mkDerivation {
+        name = "${name}";
+        src = self;
+        nativeBuildInputs = [ jdk21_headless maven makeWrapper ];
+        buildPhase = "${pkgs.maven} package";
+        installPhase= ''
+          mkdir -p $out/bin; 
+          cp ./target/${name}-${version}.jar $out/;
+          makeWrapper ${pkgs.jdk21_headless}/bin/java $out/bin/${name} \
+            --add-flags "-jar $out/${name}-${version}.jar"
+        '';
+      };
+    
+
+    devShells.${system}.default = pkgs.mkShell {
       packages = [
         pkgs.jdk21
         pkgs.maven
