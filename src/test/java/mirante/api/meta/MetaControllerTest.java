b56d45ca89f5b3ce3f9104830a106bee7b366886
jultty <juno.t@tuta.io>
2024-03-10 17:37:20 -0300

    Adapt /version basic test for auth

--- src/test/java/mirante/api/meta/MetaControllerTest.java
+++ src/test/java/mirante/api/meta/MetaControllerTest.java
@@ -4,15 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class MetaControllerTest {
 
   @Test
   void versionEndpointReturnsCurrentVersion(@Autowired TestRestTemplate template) {
-    String body = template.getForObject("/version", String.class);
-    assertThat(body).contains("v0.2.0");
+    ResponseEntity<String> response = template.withBasicAuth("spring","secret")
+      .getForEntity("/version", String.class);
+    assertThat(Objects.requireNonNull(response.getBody())).contains("v0.2.0");
+    assertEquals(response.getStatusCode(), (HttpStatus.OK));
   }
 }
